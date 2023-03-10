bf050932e0f584be865ba1193e41a43604f2a82b
Ubuntu <ubuntu@ip-172-31-47-13.eu-north-1.compute.internal>
2023-03-10 06:23:50 +0000

    version 2

--- app/app.py
+++ app/app.py
@@ -1,16 +1,87 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import * 
 from kernel4 import IDSearcher
 import time
+import flask
+import flask_login
 
 app = Flask(__name__)
 id_searcher = IDSearcher()
+app.secret_key = 'xU.Wy;*5DA8YL:[}'  # Change this!
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+users = {
+    
+    }
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <center>
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='token' id='token' placeholder='token'/>
+                <input type='submit' name='submit'/>
+               </form>
+               </center>
+               '''
+
+    email = flask.request.form['email']
+    if email in users and flask.request.form['token'] == users[email]['token']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('search_page'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized', 401
+
 
 @app.route('/')
+@flask_login.login_required
 def search_page():
+    # return flask.redirect(flask.url_for('login'))
     return render_template('search.html')
 
 @app.route('/search', methods=['POST'])
+@flask_login.login_required
 def search_ids():
     flag = False
     search_type = int(request.form['search_type'])
@@ -34,6 +105,7 @@ def search_ids():
     else:
         return render_template('search.html', error='The number of IDs must be less than 10000', flag=flag)
 
+# cp db then show 2nd db here as backup
 @app.route('/database')
 def show_database():
     try:
